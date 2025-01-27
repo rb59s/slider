d115e316f0a29e03d03b8b09432a067e88e84743
Keermelckbrugge <Julien.Keermel@icloud.com>
2025-01-27 15:55:43 +0100

    jhcje

---
+++ slide.js
@@ -0,0 +1,16 @@
+const chevron_gauche = document.querySelector('#précedent');
+const chevron_droit = document.querySelector('#suivant');
+const slide = ["brother.webp", "milky-way.webp", "montagne.webp", "montagne.webp", "oiseau.webp"];               
+let numero = 0;
+
+function changedeslide(sens){
+numero = numero + sens;
+if (numero > 3)
+    numero = 0;
+if (numero < 0)
+    numero = 3;
+console.log(slide);
+
+ document.querySelector('#slide').src = "./slide2/" + slide[numero];
+
+}
